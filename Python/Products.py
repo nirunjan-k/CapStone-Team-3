c78d107fd3f4c5000b8cdef8146dcc3091330528
nirunjan-k <154658713+nirunjan-k@users.noreply.github.com>
2023-12-24 09:33:56 +0530

    Update Products.py

--- Python/Products.py
+++ Python/Products.py
@@ -1,36 +1,21 @@
-DROP TABLE IF EXISTS STG_Products;
-select * into STG_Products from
-(select * from dbo.Products)a;
- 
-merge into Project.dbo.STG_Products sp
-using Project.dbo.Products p on
-sp.ProductID = p.ProductID
-when matched and (sp.ProductID!=p.ProductID or 
-sp.ProductName!=p.ProductName or 
-sp.SupplierID!=p.SupplierID or
-sp.CategoryID!=p.CategoryID or
-sp.QuantityPerUnit!=p.QuantityPerUnit or 
-sp.UnitPrice!=p.UnitPrice or 
-sp.UnitsInStock!=p.UnitsInStock or 
-sp.UnitsOnOrder!=p.UnitsOnOrder or 
-sp.ReorderLevel!=p.ReorderLevel or 
-sp.Discontinued!=p.Discontinued)
-then update
-set
-sp.ProductID=p.ProductID, 
-sp.ProductName=p.ProductName, 
-sp.SupplierID=p.SupplierID, 
-sp.CategoryID=p.CategoryID, 
-sp.QuantityPerUnit=p.QuantityPerUnit, 
-sp.UnitPrice=p.UnitPrice, 
-sp.UnitsInStock=p.UnitsInStock, 
-sp.UnitsOnOrder=p.UnitsOnOrder, 
-sp.ReorderLevel=p.ReorderLevel, 
-sp.Discontinued=p.Discontinued
-when not matched
-then insert values(p.ProductID, p.ProductName, p.SupplierID, p.CategoryID, 
-p.QuantityPerUnit, p.UnitPrice, p.UnitsInStock, p.UnitsOnOrder, p.ReorderLevel, p.Discontinued)
-when not matched by source
-then delete;
- 
-select * from STG_Products;
+import pyodbc
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("C:\\Technical Training\\Project\\Products.csv",quotechar="\"")
+
+df.columns = df.columns.astype(str)
+df.replace({np.inf: np.nan, -np.inf: np.nan}, inplace=True)
+df = df.fillna(0)
+
+cnxn = pyodbc.connect('''Driver=SQL Server;Server=IN3509253W1;Database=Project;Trusted_Connection=yes;''')
+cursor = cnxn.cursor()
+cursor.execute("Truncate table dbo.Products")
+
+
+for index, row in df.iterrows():
+    cursor.execute("INSERT INTO dbo.Products(ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued) values(?,?,?,?,?,?,?,?,?,?)", row.ProductID,row.ProductName,row.SupplierID,row.CategoryID,row.QuantityPerUnit,row.UnitPrice,row.UnitsInStock,row.UnitsOnOrder,row.ReorderLevel,row.Discontinued)
+
+cnxn.commit()
+print('Done')
+cursor.close()
